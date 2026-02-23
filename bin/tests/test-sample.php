c6d6a18842759ebdb798568f02ad21ef60d1072c
Athliara <156673935+Athliara@users.noreply.github.com>
2026-02-23 14:28:53 +0200

    Add files via upload

---
+++ bin/tests/test-sample.php
@@ -0,0 +1,13 @@
+<?php
+
+if (!defined('ABSPATH')) {
+	exit;
+}
+
+class SampleTest extends WP_UnitTestCase {
+
+	function testSample() {
+		// replace this with some actual testing code
+		$this->assertTrue( true );
+	}
+}
